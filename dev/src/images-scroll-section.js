e601481610dc141395ea830918a19a797bd4f2e7
AndriiKravets <tetamoed1@gmail.com>
2023-10-09 00:02:45 +0300

    added files and schema images scroll

---
+++ dev/src/images-scroll-section.js
@@ -0,0 +1,13 @@
+if (!customElements.get('s-images-scroll')) {
+
+  customElements.define('s-images-scroll', class SStepsScroll extends HTMLElement {
+      constructor() {
+        super();
+      }
+
+      connectedCallback() {
+
+      }
+    }
+  )
+}
