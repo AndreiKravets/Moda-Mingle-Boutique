847d2a6d343aa465ff237e4765f4373d8aa72290
AndriiKravets <tetamoed1@gmail.com>
2023-10-12 10:33:45 +0300

    added switch Steps

--- dev/src/images-scroll-section.js
+++ dev/src/images-scroll-section.js
@@ -13,13 +13,13 @@ if (!customElements.get('s-images-scroll')) {
       elemContainer = this.querySelector('.s-images-scroll__container')
       elemRow = this.querySelector('.s-images-scroll__row')
       elemWrapperImages = this.querySelector('.s-images-scroll__images')
-
+      stepsArray = Array.from(this.querySelectorAll('.s-images-scroll__content_step'))
       screenHeight = window.screen.availHeight
       containerHeight = this.elemContainer.offsetHeight
       rowHeight = this.screenHeight * (80 / 100)
       containerTopPosition = (this.screenHeight - this.rowHeight) / 4
       containerBottomPosition = (this.containerHeight - this.rowHeight)
-
+      activeStepIndex = 0
 
       addStyles() {
         this.elemContainer.style.height = this.containerHeight + 'px'
@@ -47,8 +47,18 @@ if (!customElements.get('s-images-scroll')) {
         }
       }
 
+      switchSteps() {
+        let stepIndex = Math.abs(Math.round((this.elemContainer.offsetHeight + this.elemContainer.getBoundingClientRect().y) / (this.elemContainer.offsetHeight / this.stepsArray.length) - this.stepsArray.length))
+        if (this.elemContainer.getBoundingClientRect().y < 0 && this.elemContainer.getBoundingClientRect().y > -(this.containerHeight - this.rowHeight) && this.activeStepIndex != stepIndex) {
+          this.activeStepIndex = stepIndex
+          this.stepsArray[stepIndex -1] == undefined ? false : this.stepsArray[stepIndex -1].classList = 's-images-scroll__content_step s-images-scroll__content_step--top'
+          this.stepsArray[stepIndex] == undefined ? false : this.stepsArray[stepIndex].classList = 's-images-scroll__content_step s-images-scroll__content_step--active'
+          this.stepsArray[stepIndex + 1] == undefined ? false : this.stepsArray[stepIndex +1].classList = 's-images-scroll__content_step s-images-scroll__content_step--bottom'
+        }
+      }
+
       scrollEvent() {
-        window.addEventListener('scroll', () => (this.toggleFixedSection(), this.translateImages()))
+        window.addEventListener('scroll', () => (this.toggleFixedSection(), this.translateImages(), this.switchSteps()))
       }
     }
   )
