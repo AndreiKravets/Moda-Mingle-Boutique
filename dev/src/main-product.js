76f6c096e079634bacae1055acc6b61bc0f8906e
AndriiKravets <tetamoed1@gmail.com>
2023-10-15 22:10:56 +0300

    added slider

--- dev/src/main-product.js
+++ dev/src/main-product.js
@@ -5,6 +5,8 @@ customElements.define('main-product', class MProduct extends HTMLElement {
 
     variants = JSON.parse(this.getAttribute('data-variants'))
     options = this.querySelectorAll('.variants-option')
+    slider
+    sliderNav
 
     connectedCallback() {
       this.options.forEach(option => {
@@ -14,7 +16,30 @@ customElements.define('main-product', class MProduct extends HTMLElement {
       })
       this.querySelector('.add_to_cart').addEventListener('click', this.addToCart.bind(this))
       this.changeCount()
+      this.initSliders()
     }
+
+    initSliders() {
+      const settings = {
+        contain: false,
+        wrapAround: true,
+        pageDots: false,
+        prevNextButtons: false,
+        percentPosition: false
+      }
+      this.slider = new Flickity(this.querySelector('.main-product__gallery-slider'), settings);
+
+      const settingsNav = {
+        asNavFor: this.querySelector('.main-product__gallery-slider'),
+        contain: false,
+        wrapAround: true,
+        pageDots: false,
+        prevNextButtons: false,
+        percentPosition: false
+      }
+      this.sliderNav = new Flickity(this.querySelector('.main-product__gallery-slider_nav'), settingsNav);
+    }
+
 
     addToCart() {
       const form = this.querySelector('.shopify-product-form')
@@ -30,44 +55,49 @@ customElements.define('main-product', class MProduct extends HTMLElement {
       });
     }
 
-  changeCount() {
-    const productQuantity = this.querySelector('#product-quantity')
-    const count = this.querySelector('.product-count__value')
-    const minus = this.querySelector('.product-count__minus')
-    const plus = this.querySelector('.product-count__plus')
+    changeCount() {
+      const productQuantity = this.querySelector('#product-quantity')
+      const count = this.querySelector('.product-count__value')
+      const minus = this.querySelector('.product-count__minus')
+      const plus = this.querySelector('.product-count__plus')
 
-    minus.addEventListener('click', () => {
-      count.value = count.value - 1
-      productQuantity.value = count.value
-    })
-    plus.addEventListener('click', () => {
-      count.value = +count.value + 1
-      productQuantity.value = count.value
-    })
-  }
+      minus.addEventListener('click', () => {
+        count.value = count.value - 1
+        productQuantity.value = count.value
+      })
+      plus.addEventListener('click', () => {
+        count.value = +count.value + 1
+        productQuantity.value = count.value
+      })
+    }
 
-  variantOnClick() {
-    let tempVariant = ''
-    this.options.forEach(variant => {
-        tempVariant += `${variant.querySelector('input:checked').value} / `
-      }
-    )
-    tempVariant = tempVariant.slice(0, -3)
+    variantOnClick() {
+      let tempVariant = ''
+      this.options.forEach(variant => {
+          tempVariant += `${variant.querySelector('input:checked').value} / `
+        }
+      )
+      tempVariant = tempVariant.slice(0, -3)
 
-    let currentVariantId = ''
-    this.variants.map((variant) => {
-      if (variant.title == tempVariant) {
-        currentVariantId = variant.id
-        history.replaceState(null, null, window.location.pathname + '?variant=' + variant.id)
-        this.querySelector('.featured-image').setAttribute('src', variant.featured_image.src)
-        const currency = this.querySelector('.main-product__content_price').innerHTML[0]
-        this.querySelector('.main-product__content_price').innerHTML = `${currency + (variant.price / 100).toFixed(2)}`
-        variant.available ? this.querySelector('.main-product').classList.remove('sold-out') : this.querySelector('.main-product').classList.add('sold-out')
-      }
-    })
+      let currentVariantId = ''
+      this.variants.map((variant) => {
+        if (variant.title == tempVariant) {
+          currentVariantId = variant.id
+          history.replaceState(null, null, window.location.pathname + '?variant=' + variant.id)
+          const currency = this.querySelector('.main-product__content_price').innerHTML[0]
+          this.querySelector('.main-product__content_price').innerHTML = `${currency + (variant.price / 100).toFixed(2)}`
+          variant.available ? this.querySelector('.main-product').classList.remove('sold-out') : this.querySelector('.main-product').classList.add('sold-out')
 
-    this.querySelector('#variant-select').value = currentVariantId
+          this.querySelectorAll('.main-product__gallery-slider .flickity-slider img').forEach((img, index) => {
+            if (img.getAttribute('src') == variant.featured_image.src && this.slider.selectedIndex != index) {
+              this.slider.select(index);
+            }
+          })
 
-  }
+        }
+      })
+
+      this.querySelector('#variant-select').value = currentVariantId
+    }
   }
 )
