0429d428934529ee78480b30311c8c8a31fd3fb2
AndriiKravets <tetamoed1@gmail.com>
2023-10-15 15:39:41 +0300

    added variants/add to cart

--- dev/src/main-product.js
+++ dev/src/main-product.js
@@ -2,5 +2,72 @@ customElements.define('main-product', class MProduct extends HTMLElement {
     constructor() {
       super();
     }
+
+    variants = JSON.parse(this.getAttribute('data-variants'))
+    options = this.querySelectorAll('.variants-option')
+
+    connectedCallback() {
+      this.options.forEach(option => {
+        option.addEventListener('change', (e) => {
+          this.variantOnClick()
+        })
+      })
+      this.querySelector('.add_to_cart').addEventListener('click', this.addToCart.bind(this))
+      this.changeCount()
+    }
+
+    addToCart() {
+      const form = this.querySelector('.shopify-product-form')
+      const formUrl = window.theme.shopUrl + window.theme.routes.cart_add_url
+      const formBody = {
+        method: 'POST',
+        body: new FormData(form)
+      }
+      fetch(formUrl, formBody).then((res) => {
+        if (res.ok) {
+          PubSub.publish(PubSub.EVENTS.cartUpdated)
+        }
+      });
+    }
+
+  changeCount() {
+    const productQuantity = this.querySelector('#product-quantity')
+    const count = this.querySelector('.product-count__value')
+    const minus = this.querySelector('.product-count__minus')
+    const plus = this.querySelector('.product-count__plus')
+
+    minus.addEventListener('click', () => {
+      count.value = count.value - 1
+      productQuantity.value = count.value
+    })
+    plus.addEventListener('click', () => {
+      count.value = +count.value + 1
+      productQuantity.value = count.value
+    })
+  }
+
+  variantOnClick() {
+    let tempVariant = ''
+    this.options.forEach(variant => {
+        tempVariant += `${variant.querySelector('input:checked').value} / `
+      }
+    )
+    tempVariant = tempVariant.slice(0, -3)
+
+    let currentVariantId = ''
+    this.variants.map((variant) => {
+      if (variant.title == tempVariant) {
+        currentVariantId = variant.id
+        history.replaceState(null, null, window.location.pathname + '?variant=' + variant.id)
+        this.querySelector('.featured-image').setAttribute('src', variant.featured_image.src)
+        const currency = this.querySelector('.main-product__content_price').innerHTML[0]
+        this.querySelector('.main-product__content_price').innerHTML = `${currency + (variant.price / 100).toFixed(2)}`
+        variant.available ? this.querySelector('.main-product').classList.remove('sold-out') : this.querySelector('.main-product').classList.add('sold-out')
+      }
+    })
+
+    this.querySelector('#variant-select').value = currentVariantId
+
+  }
   }
 )
